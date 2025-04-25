a954284484bae1495511a494f28be72c8424d1e4
Ana Carolina Machado <anacarolinamachado366@gmail.com>
2025-04-24 21:54:49 -0300

    CRIACAO DO FRMPRINCIPAL

---
+++ SenacFoodsSolucao/SenacFoods/FrmLogin.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace SenacFoods
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            btnEntrar = new Button();
+            txtSenha = new TextBox();
+            txtLogin = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(300, 568);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(btnEntrar);
+            panel2.Controls.Add(txtSenha);
+            panel2.Controls.Add(txtLogin);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(label1);
+            panel2.Dock = DockStyle.Right;
+            panel2.Location = new Point(353, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(699, 568);
+            panel2.TabIndex = 1;
+            // 
+            // btnEntrar
+            // 
+            btnEntrar.FlatAppearance.BorderSize = 0;
+            btnEntrar.FlatAppearance.MouseDownBackColor = Color.White;
+            btnEntrar.FlatAppearance.MouseOverBackColor = Color.Silver;
+            btnEntrar.FlatStyle = FlatStyle.Flat;
+            btnEntrar.Font = new Font("Rockwell", 18F);
+            btnEntrar.ForeColor = Color.White;
+            btnEntrar.Location = new Point(148, 355);
+            btnEntrar.Name = "btnEntrar";
+            btnEntrar.Size = new Size(446, 61);
+            btnEntrar.TabIndex = 2;
+            btnEntrar.Text = "Entrar";
+            btnEntrar.UseVisualStyleBackColor = true;
+            // 
+            // txtSenha
+            // 
+            txtSenha.BackColor = Color.OrangeRed;
+            txtSenha.BorderStyle = BorderStyle.None;
+            txtSenha.Font = new Font("Rockwell", 18F);
+            txtSenha.ForeColor = Color.White;
+            txtSenha.Location = new Point(148, 251);
+            txtSenha.Name = "txtSenha";
+            txtSenha.Size = new Size(435, 43);
+            txtSenha.TabIndex = 0;
+            txtSenha.Text = "SENHA";
+            // 
+            // txtLogin
+            // 
+            txtLogin.BackColor = Color.OrangeRed;
+            txtLogin.BorderStyle = BorderStyle.None;
+            txtLogin.Font = new Font("Rockwell", 18F);
+            txtLogin.ForeColor = Color.White;
+            txtLogin.Location = new Point(148, 146);
+            txtLogin.Name = "txtLogin";
+            txtLogin.Size = new Size(435, 43);
+            txtLogin.TabIndex = 0;
+            txtLogin.Text = "USUÁRIO";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(139, 262);
+            label2.Name = "label2";
+            label2.Size = new Size(455, 48);
+            label2.TabIndex = 1;
+            label2.Text = "__________________\u001f\u001f\u001f____________";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(139, 156);
+            label1.Name = "label1";
+            label1.Size = new Size(455, 48);
+            label1.TabIndex = 1;
+            label1.Text = "__________________\u001f\u001f\u001f____________";
+            // 
+            // FrmLogin
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.OrangeRed;
+            ClientSize = new Size(1052, 568);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "FrmLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private TextBox txtLogin;
+        private TextBox txtSenha;
+        private Label label1;
+        private Label label2;
+        private Button btnEntrar;
+    }
+}
---
+++ SenacFoodsSolucao/SenacFoods/FrmPrincipal.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace SenacFoods
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.ForeColor = Color.White;
+            groupBox1.Location = new Point(59, 72);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(974, 324);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 255, 128);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseDownBackColor = Color.White;
+            button1.FlatAppearance.MouseOverBackColor = Color.Gray;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Rockwell", 16F);
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(66, 89);
+            button1.Name = "button1";
+            button1.Size = new Size(172, 155);
+            button1.TabIndex = 0;
+            button1.Text = "Cardápio";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(128, 255, 128);
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatAppearance.MouseDownBackColor = Color.White;
+            button2.FlatAppearance.MouseOverBackColor = Color.Gray;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Rockwell", 16F);
+            button2.ForeColor = Color.Black;
+            button2.Location = new Point(289, 89);
+            button2.Name = "button2";
+            button2.Size = new Size(172, 155);
+            button2.TabIndex = 0;
+            button2.Text = "Comanda";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.DeepSkyBlue;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatAppearance.MouseDownBackColor = Color.White;
+            button3.FlatAppearance.MouseOverBackColor = Color.Gray;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Rockwell", 16F);
+            button3.ForeColor = Color.Black;
+            button3.Location = new Point(516, 89);
+            button3.Name = "button3";
+            button3.Size = new Size(172, 155);
+            button3.TabIndex = 0;
+            button3.Text = "Pedido Cozinha";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(255, 128, 128);
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatAppearance.MouseDownBackColor = Color.White;
+            button4.FlatAppearance.MouseOverBackColor = Color.Gray;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Rockwell", 16F);
+            button4.ForeColor = Color.Black;
+            button4.Location = new Point(745, 89);
+            button4.Name = "button4";
+            button4.Size = new Size(172, 155);
+            button4.TabIndex = 0;
+            button4.Text = "Usuários";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // FrmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(1091, 500);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "FrmPrincipal";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "FrmPrincipal";
+            groupBox1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button button1;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+    }
+}
